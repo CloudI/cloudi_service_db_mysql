6fed34d09a0d364f834698d3d07cfa19bc5766be
Michael Truog <mjtruog@gmail.com>
2014-10-30 17:54:25 -0700

    Update hex package.

---
+++ mix.exs
@@ -0,0 +1,32 @@
+defmodule CloudIServiceDbMysql do
+  use Mix.Project
+
+  def project do
+    [app: :cloudi_service_db_mysql,
+     version: "1.4.0-rc.4",
+     language: :erlang,
+     description: description,
+     package: package,
+     deps: deps]
+  end
+
+  defp deps do
+    [{:emysql,
+      [git: "https://github.com/okeuday/emysql.git",
+       branch: "v0.4.1_CloudI"]},
+     {:cloudi_core, "~> 1.4.0-rc.4"}]
+  end
+
+  defp description do
+    "Erlang/Elixir Cloud Framework MySQL Service"
+  end
+
+  defp package do
+    [files: ~w(src doc test rebar.config README.markdown),
+     contributors: ["Michael Truog"],
+     licenses: ["BSD"],
+     links: %{"Website" => "http://cloudi.org",
+              "GitHub" => "https://github.com/CloudI/" <>
+                          "cloudi_service_db_mysql"}]
+   end
+end
