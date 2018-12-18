a1f1a6ffd8c7fba18b24c42e6ad295adfaeaa900
Michael Truog <mjtruog@protonmail.com>
2018-12-17 18:34:40 -0800

    Version 1.7.5.

--- mix.exs
+++ mix.exs
@@ -6,7 +6,7 @@ defmodule CloudIServiceDbMysql do
 
   def project do
     [app: :cloudi_service_db_mysql,
-     version: "1.7.4",
+     version: "1.7.5",
      language: :erlang,
      erlc_options: [
        {:d, :erlang.list_to_atom('ERLANG_OTP_VERSION_' ++ :erlang.system_info(:otp_release))},
@@ -33,7 +33,7 @@ defmodule CloudIServiceDbMysql do
     [{:emysql,
       [git: "https://github.com/okeuday/emysql.git",
        branch: "v0.4.2_CloudI"]},
-     {:cloudi_core, "~> 1.7.4"}]
+     {:cloudi_core, "~> 1.7.5"}]
   end
 
   defp description do
